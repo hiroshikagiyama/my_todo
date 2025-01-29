58953c793feb86b89212676db845be15b77b2245
kagi <hiroshi_kagiyama_aa@mail.toyota.co.jp>
2025-01-29 17:03:27 +0900

    yukaさんが変更 test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,14 @@
 import '@testing-library/jest-dom/vitest';
-import { expect, test } from 'vitest'
+import { vitest,expect, test } from 'vitest'
 import {render, screen} from "@testing-library/react";
 import App from "./App.tsx";
 
+const stubFetch = vitest.hoisted(() => vitest.fn())
+vitest.stubGlobal("fetch", () => ({json: stubFetch}))
+
 test('it renders app', () => {
+    stubFetch.mockResolvedValue([])
+
     render(<App />)
     screen.debug()
     expect(screen.getByText("My Todo")).toBeInTheDocument()
